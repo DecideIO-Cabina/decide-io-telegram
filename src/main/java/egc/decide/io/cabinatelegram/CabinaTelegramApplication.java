f1a6ac8b0d2ff9894ef9b1a44bc31e8336bae1bc
GBC <GBC@tier1.es>
2018-11-26 11:11:37 +0100

    Generado el proyecto de Spring Boot

---
+++ src/main/java/egc/decide/io/cabinatelegram/CabinaTelegramApplication.java
@@ -0,0 +1,12 @@
+package egc.decide.io.cabinatelegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinaTelegramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabinaTelegramApplication.class, args);
+	}
+}
