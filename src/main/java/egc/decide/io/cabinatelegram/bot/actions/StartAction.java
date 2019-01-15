1078f169391148403d534cd6a533316541d8f944
gbandres98 <gbandres98@gmail.com>
2019-01-15 08:34:54 +0100

    Revert "(#99) Cambiado el mensaje de saludo."
    This reverts commit 34a235724ce3522cad8b61d750201ca22e5e6068.

--- src/main/java/egc/decide/io/cabinatelegram/bot/actions/StartAction.java
+++ src/main/java/egc/decide/io/cabinatelegram/bot/actions/StartAction.java
@@ -41,7 +41,7 @@ public class StartAction implements DecideBotAction {
 		
 		return new SendMessage().setChatId(update.getMessage().getChatId())
 				.setReplyMarkup(keyboard)
-				.setText("Bienvenido a la cabina de votación de Telegram para Decide-Defensa");
+				.setText("Bienvenido a la cabina de votación de Telegram para Decide");
 	}
 	
 	private BotApiMethod<?> login(Update update, UserSession userSession) {
